627024ec48f3c3885cdfb896437e6445ccd538f5
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-12-19 17:39:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,10 +56,10 @@ packages = [
 
 setup(name='hcord',
       author='HexyeDEV',
-      url='https://github.com/HexyeDEV/hcord',
+      url='https://github.com/HCord/hcord',
       project_urls={
         "Documentation": "https://hcord.readthedocs.io/en/latest/",
-        "Issue tracker": "https://github.com/HexyeDEV/hcord/issues",
+        "Issue tracker": "https://github.com/HCord/hcord/issues",
       },
       version=version,
       packages=packages,
